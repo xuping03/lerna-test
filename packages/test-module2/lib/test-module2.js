42dfad634c3fa4b9f5a952cfef97345898a0acd0
xuping04 <xuping04@baidu.com>
2023-09-12 11:40:20 +0800

    test2

--- packages/test-module2/lib/test-module2.js
+++ packages/test-module2/lib/test-module2.js
@@ -5,6 +5,6 @@ const testModule1 = require('test-module1');
 function testModule2() {
   return 'Hello from testModule2';
 }
-
+testModule1();
 console.log(testModule1());
 console.log(testModule2());
